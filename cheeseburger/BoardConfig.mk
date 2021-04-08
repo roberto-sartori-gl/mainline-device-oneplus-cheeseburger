35f2f7dffb9a4b6c0d7274cb924cf13ecf6958d2
Roberto Sartori <roberto.sartori.android@gmail.com>
2021-04-08 20:42:04 +0200

    cheeseburger: fix boot_devices
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- cheeseburger/BoardConfig.mk
+++ cheeseburger/BoardConfig.mk
@@ -10,7 +10,7 @@ BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 BOARD_KERNEL_CMDLINE     := console=ttyMSM0,115200n8 androidboot.console=ttyMSM0
-BOARD_KERNEL_CMDLINE     += androidboot.hardware=cheeseburger androidboot.boot_devices=soc@0/1d84000.ufshc
+BOARD_KERNEL_CMDLINE     := androidboot.hardware=cheeseburger androidboot.boot_devices=soc@0/1da4000.ufshc
 BOARD_KERNEL_CMDLINE     += firmware_class.path=/vendor/firmware/ init=/init printk.devkmsg=on
 BOARD_KERNEL_CMDLINE     += deferred_probe_timeout=30
 BOARD_KERNEL_CMDLINE     += fw_devlink=permissive
